584815c3466a2aa24002c2bf7cbdce7e716a2a57
Muriithi Ian <imuriithiian@gmail.com>
2022-09-25 15:16:09 +0300

    completed

--- src/components/App.js
+++ src/components/App.js
@@ -9,12 +9,12 @@ console.log(user);
 
 function App() {
   return (
-    <div>
-      <NavBar />
-      <Home />
-      <About />
-    </div>
-  );
+		<div>
+			<NavBar />
+			<Home name={user.name} city={user.city} color={user.color} />
+			<About bio={user.bio} links={user.links} />
+		</div>
+	);
 }
 
 export default App;
